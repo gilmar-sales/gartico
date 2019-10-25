50ff0610f7752888e6a249a60977fdae8cfe56b4
gilmarxd <gilmar_custodio_@hotmail.com>
2019-10-24 22:43:38 -0300

    socketio initial setup and remote object(server side)

--- index.py
+++ index.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
+from flask_socketio import SocketIO, emit, join_room, leave_room
 
 app = Flask(__name__)
+socketio = SocketIO(app)
 
 @app.route('/')
 def index():
@@ -9,3 +11,22 @@ def index():
 @app.route('/room/<int:room_id>')
 def room(room_id):
     return render_template('room.html', room_id = room_id)
+
+@socketio.on('join')
+def on_join(data):
+    username = data['username']
+    room = data['room']
+    join_room(room)
+    print(username + ' has entered the room: ' + room)
+
+@socketio.on('leave')
+def on_leave(data):
+    username = data['username']
+    room = data['room']
+    leave_room(room)
+    print(username + ' has left the room: ' + room)
+
+# response to remote object call
+@socketio.on("request invoke")
+def invoke(data):
+    emit("invoke method", data, include_self=False, room = data['room'])
