396cdb4c943b03202c0d1ed732a73a7f33f5e5bd
gilmarxd <gilmar_custodio_@hotmail.com>
2019-11-27 21:10:44 -0300

    Add TCP

--- static/js/rmi.js
+++ static/js/rmi.js
@@ -2,9 +2,9 @@ class RemoteObject {
     // The handler to make the proxy emit the request of the method 
     static handler = {
         apply: function(target, thisArg, argumentsList) {
-            if(thisArg.observer)
+            if(thisArg.listener)
                 return;
-            thisArg.socket.emit("request invoke", {room: thisArg.room, method: target.name, args: argumentsList});
+            thisArg.socket.emit("request invoke", {room: thisArg.room, packet: thisArg.actual_packet++, method: target.name, args: argumentsList});
 
             return target.apply(thisArg, argumentsList);
         }
@@ -13,12 +13,13 @@ class RemoteObject {
     constructor(protocol, domain, port, room) {
         this.socket = io.connect(protocol + '//' + domain + ':' + port);
         this.room = room;
-        this.observer = false;
+        this.listener = true;
 
         //tcp protocol
-        this.packets = [];
-        this.actual_packet = -1; // number of the last packet processed
-
+        this.packets = {};
+        this.processed_packet = 0;
+        
+        this.actual_packet = 0; // number of the last packet processed
         // Backup of the methods that cannot be accessed throught the proxy
         this.__targets__ = [];
 
@@ -33,18 +34,50 @@ class RemoteObject {
 
         // When a request is called by the server, apply the method
         this.socket.on('invoke method' , data => {
-            if(this.__targets__[data['method']])
-                this.__targets__[data['method']].apply(this, data['args']);
+            
+            if(data['packet'] > this.processed_packet) {
+                this.packets[data['packet']] = data;
+                console.log("armazenando pacote: " + data['packet']+ " atual: " + this.processed_packet);
+            }
+            else {
+                for(const packet in this.packets) {
+                    if(packet > this.processed_packet)
+                        break;
+
+                    this.__targets__[this.packets[packet]['method']].apply(this, this.packets[packet]['args']);
+                    this.processed_packet++;
+                    this.actual_packet++;
+                    delete this.packets[packet];
+                    console.log("aplicando pacote");
+                }
+
+                console.log("aplicando pacote");
+
+                if(this.__targets__[data['method']])
+                    this.__targets__[data['method']].apply(this, data['args']);
+                
+                this.processed_packet++;
+                this.actual_packet++;
+
+            }
         });
 
         // Make the proxy for each method apply
         var current = this.__proto__;
 
-        Object.getOwnPropertyNames(current).forEach((name) => {
-            this.__targets__[name] = current[name];
-            if (typeof current[name] === 'function') {
-                current[name] = new Proxy(current[name], RemoteObject.handler);
-            }
-        });
-    };
+        while(current['constructor']['name'] != "Object") {
+            Object.getOwnPropertyNames(current).forEach((name) => {
+                this.__targets__[name] = current[name];
+                if (typeof current[name] === 'function') {
+                    current[name] = new Proxy(current[name], RemoteObject.handler);
+                }
+            });
+            
+            current = current.__proto__;
+        }
+    }
+
+    setListener(value) {
+        this.listener = value;
+    }
 }
